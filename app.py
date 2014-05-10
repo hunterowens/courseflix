053e6687b5938469de53eb53d6ca1eb710c73853
hunterowens <owens.hunter@gmail.com>
2014-05-10 16:16:27 +0800

    models

--- app.py
+++ app.py
@@ -10,10 +10,39 @@ db = SQLAlchemy(app)
 
 ## DATA MODELS ## 
 
-class class_listing(db.Model):
+class ClassListing(db.Model):
     __tablename__ = 'classes'
     dept = db.Column(db.String(4), unique = False)
     number = db.Column(db.Integer, unique = True)
     name = db.Column(db.String(255), primary_key=True)
 
+class Adjective(db.Model):
+    __tablename__ = 'adjectives'
+    name = db.Column(db.String(255), primary_key=True)
+
+class TimePeriod(db.Model):
+    __tablename__ = 'times'
+    time = db.Column(db.String(255), primary_key=True)
+
+class Region(db.Model):
+    __tablename__ = 'regions'
+    region = db.Column(db.String(50), primary_key=True)
+
+class Description(db.Model):
+    __tablename__ = 'desc'
+    id = db.Column(db.Integer, primary_key=True)
+    buzzword = db.Column(db.String(50))
+    reality = db.Column(db.String(50))
+
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    adjective = db.Column(db.String(100))
+    activty = db.Column(db.String(100))
+
+class Audience(db.Model):
+    __tablename__ = 'audiences'
+    id = db.Column(db.Integer, primary_key=True)
+    sterotype = db.Column(db.String(100))
+    description = db.Column(db.String(255))
 
